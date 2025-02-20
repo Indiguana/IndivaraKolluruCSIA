aab5b4fa9d63f7099bed7431d8eb83097ee6dcd9
indiguana <indiguana@MacBook-Pro-135.local>
2025-02-20 14:30:44 -0600

    updated with redminers

--- home_page.py
+++ home_page.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from energyGraph import GraphPage
+from reminders import RemindersPage
 
 class HomePage(tk.Frame):
     def __init__(self, root, controller):
@@ -13,14 +15,13 @@ class HomePage(tk.Frame):
         tk.Button(self, text="Settings", font=("Arial", 12), command=self.show_settings_page).pack(pady=5)
 
     def show_graph_page(self):
-        from energyGraph import GraphPage  # Import here to avoid circular import
         self.controller.switch_frame(GraphPage)
     
     def show_comparison_page(self):
         pass  # Placeholder for comparison page navigation
     
     def show_update_reminders(self):
-        pass  # Placeholder for reminders update navigation
+        self.controller.switch_frame(RemindersPage)
     
     def show_settings_page(self):
         pass  # Placeholder for settings navigation
