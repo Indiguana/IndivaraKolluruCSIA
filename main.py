f57ca4634d6713f3f8c2326b2fe58175f441a58f
indiguana <indiguana@MacBook-Pro-135.local>
2025-02-22 14:23:25 -0600

    updated main to work with all of the new features

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from home_page import HomePage
 from energyGraph import GraphPage
 from reminders import RemindersPage
+from neighborhood import NeighborhoodPage
+from settings import SettingsPage  # Fixed Import Issue
+
 
 class EnergyApp:
     def __init__(self, root):
@@ -9,19 +12,30 @@ class EnergyApp:
         self.root.title("Energy Efficient Home Budgeting")
         self.root.geometry("800x600")
         self.root.configure(bg="#87CEFA")
+
+
         self.current_frame = None
         self.show_home_page()
 
+
     def switch_frame(self, frame_class):
+        """Destroy current frame and load a new one"""
         if self.current_frame:
             self.current_frame.destroy()
         self.current_frame = frame_class(self.root, self)
         self.current_frame.pack(fill="both", expand=True)
 
+
     def show_home_page(self):
         self.switch_frame(HomePage)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = EnergyApp(root)
     root.mainloop()
+
+
+
+
+
